80a13dd90186d37c93bb7aacaa65406997bc0a57
Nabil <nabilrdhwn@gmail.com>
2018-10-13 21:24:29 -0700

    Added image verification (ends with .jpg or .png)

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var loading = document.getElementsByClassName("loading")[0]
+
 // Initialize the Image Classifier method with MobileNet
 const classifier = ml5.imageClassifier('MobileNet', function () {
     console.log('Model Loaded!');
@@ -8,8 +10,11 @@ var app = new Vue({
 
     data() {
         return {
-            imgsrc: "https://i.imgur.com/m4bkC5g.jpg",
-            repo:{
+
+            // MY FACE!!!
+            imgsrc: "http://2.bp.blogspot.com/_pVSSnrgAXPY/TGDYQlH1_XI/AAAAAAAAAA4/qHZk2bd8gi0/s1600/Photo-0189.jpg",
+            
+            repo: {
                 link: "https://github.com/rdhwnsh/ml5vue",
                 author: "Nabil Ridhwanshah"
             }
@@ -19,54 +24,75 @@ var app = new Vue({
 
     methods: {
         changeImage() {
-            var image = document.getElementById('image');
 
-            image.src = this.imgsrc
-            // SET TIMEOUT FOR CALLBACK BECAUSE THE NEW IMAGE WILL NOT BE LOADED YET AND WILL CLASSIFY THE OLD IMAGE INSTEAD OF THE NEW ONE
-            setTimeout(() => {
-                this.classify_image(image)
-            }, 1)
+
+            if (this.imgsrc.indexOf("jpg") > 0 || this.imgsrc.indexOf("png") > 0) {
+                // Change the not valid image file to none
+                document.getElementsByClassName("notvalidimage")[0].style.display = "none";
+
+                var image = document.getElementById('image');
+
+                image.src = this.imgsrc
+                // SET TIMEOUT FOR CALLBACK BECAUSE THE NEW IMAGE WILL NOT BE LOADED YET AND WILL CLASSIFY THE OLD IMAGE INSTEAD OF THE NEW ONE
+                setTimeout(() => {
+                    this.classify_image(image)
+                }, 1)
+            } else {
+
+                // DISPLAY THE NOT VALID IMAGE
+                document.getElementsByClassName("notvalidimage")[0].style.display = "initial";
+                console.log("error")
+            }
+
         },
 
         classify_image(image) {
+
+            // Change the loading display to none
+            document.getElementsByClassName("loading")[0].style.display = "none"
+
             classifier.predict(image, function (err, results) {
-                
+
+                if (err) {
+                    console.log(err);
+                }
+
                 // Result for default (0)
                 var result = results[0].className;
                 document.getElementById("result").innerHTML = result
-                console.log(results)
-                
+
                 // Probability in % for default (0)
                 var probability = Math.floor(results[0].probability * 100) + "%";
-                console.log(probability)
                 document.getElementById("probability").innerHTML = probability
 
                 // =================================================
-                
+
                 // Result for 1
-                var result = results[1].className;
-                document.getElementById("result1").innerHTML = result
-                console.log(results)
-                
+                var result1 = results[1].className;
+                document.getElementById("result1").innerHTML = result1
+
                 // Probability in % for 1
-                var probability = Math.floor(results[1].probability * 100) + "%";
-                console.log(probability)
-                document.getElementById("probability1").innerHTML = probability
+                var probability1 = Math.floor(results[1].probability * 100) + "%";
+                document.getElementById("probability1").innerHTML = probability1
 
                 // Result for 2
-                var result = results[02].className;
-                document.getElementById("result2").innerHTML = result
-                console.log(results)
-                
+                var result2 = results[02].className;
+                document.getElementById("result2").innerHTML = result2
+
                 // Probability in % for 2
-                var probability = Math.floor(results[02].probability * 100) + "%";
-                console.log(probability)
-                document.getElementById("probability2").innerHTML = probability
+                var probability2 = Math.floor(results[02].probability * 100) + "%";
+                document.getElementById("probability2").innerHTML = probability2
             });
         }
     },
 
     mounted() {
         this.changeImage()
+
+        // Change the loading display to none
+        document.getElementsByClassName("loading")[0].style.display = "none";
+
+        // Change the not valid image file to none
+        document.getElementsByClassName("notvalidimage")[0].style.display = "none";
     }
 })
