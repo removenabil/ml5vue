ae520f04c6403119aac89271adc9f7bb65e24df8
Nabil <nabilrdhwn@gmail.com>
2018-10-13 20:08:41 -0700

    Added vue.js with ml5

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
+// Initialize the Image Classifier method with MobileNet
+const classifier = ml5.imageClassifier('MobileNet', function () {
+    console.log('Model Loaded!');
+});
+
 var app = new Vue({
     el: ".app",
-    
-    data(){
-        return{
-            name: "",
+
+    data() {
+        return {
+            imgsrc: "https://i.imgur.com/m4bkC5g.jpg",
+
+            classification: "",
+
         }
     },
 
+    methods: {
+        changeImage() {
+            var image = document.getElementById('image');
+
+            image.src = this.imgsrc
+            // SET TIMEOUT FOR CALLBACK BECAUSE THE NEW IMAGE WILL NOT BE LOADED YET AND WILL CLASSIFY THE OLD IMAGE INSTEAD OF THE NEW ONE
+            setTimeout(() => {
+                this.classify_image(image)
+            }, 1)
+        },
+
+        classify_image(image) {
+            classifier.predict(image, function (err, results) {
+                
+                // Result
+                var result = results[0].className;
+                document.getElementById("result").innerHTML = result
+                console.log(results)
+                
+                // Probability in %
+                var probability = Math.floor(results[0].probability * 100) + "%";
+                console.log(probability)
+                document.getElementById("probability").innerHTML = probability
+            });
+        }
+    },
+
+    mounted() {
+        this.changeImage()
+    }
 })
